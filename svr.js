ba55f2ba637171249fd8b306af2b0f5c7b41d925
prayer622 <prayer622@gmail.com>
2023-10-09 21:57:26 +0900

    update
    selected chart updated code

--- svr.js
+++ svr.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const path = require('path');
 const static = require('serve-static');
 const dbconfig = require('./config/dbconfig.json');
+const bodyParser = require('body-parser');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
@@ -15,6 +16,7 @@ const pool = mysql.createPool({
 })
     
     const app = express();
+    app.use(bodyParser.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     
@@ -22,6 +24,10 @@ const pool = mysql.createPool({
     app.get('/', (req, res)=>{ 
         res.sendFile(__dirname+'/public/main.html')
     })
+    app.get('/main2', (req, res)=>{ 
+        res.sendFile(__dirname+'/public/main2.html')
+    })
+
 
     app.post('/chartdatafromdb', (req, res) => {
         console.log('chartdatafromdb 호출됨');
@@ -43,6 +49,7 @@ const pool = mysql.createPool({
             const exec = conn.query('SELECT `temperature`, `reg_date` FROM `building_temperature` ORDER BY `reg_date` ASC', (err, rows) => {
                 if (err) {
                     console.log('MySQL 쿼리 에러');
+                    res.status(500).json({ result: 'error' });
                     res.json(resData);
                     return;
                 }
@@ -62,10 +69,49 @@ const pool = mysql.createPool({
     });
 
 
+
+    app.post('/chartdatafromdbwithid', (req, res) => {
+        console.log('chartdatafromdb 호출됨');
+        const event = req.body.bid;
+        console.log('bid is', event);
+
+        pool.getConnection((err, conn) => {
+            const resData = {
+                result: 'error',
+                temp: [],
+                reg_date: []
+            };
+            if (err) {
+                conn.release();
+                console.log('MySQL 커넥션 에러 발생');
+                res.json(resData);
+                return;
+            }
+             const exec = conn.query('SELECT `temperature`, `reg_date` FROM `building_temperature` WHERE `building_id` = ? ORDER BY `reg_date` ASC;' , [event],            
+            (err, rows) => {
+                if (err) {
+                    console.log('MySQL 쿼리 에러');
+                    res.status(500).json({ result: 'error' });
+                    res.json(resData);
+                    return;
+                }
+
+                if (rows[0]) {
+                    resData.result = 'ok';
+                    rows.forEach((value) => {
+                        resData.temp.push(value.temperature);
+                        resData.reg_date.push(value.reg_date);
+                    });
+                }
+    
+                res.json(resData);
+                conn.release(); // 커넥션 해제는 응답을 보낸 후에 처리해야 합니다.
+            });
+        });
+    });
+
+
     app.listen(3040, ()=>{
         console.log('3040번 듣고있습니다.');
     });
     
-
-
-
